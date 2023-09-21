4098b92f7ba1d518d02ea72e604d1a7f10347394
mignrod <mignrod@gmail.com>
2023-09-20 21:05:50 -0400

    team

--- discount.py
+++ discount.py
@@ -1,5 +1,44 @@
-# Import the datetime class from the datetime module
+"""
+You work for a retail store that wants to increase sales on Tuesday
+and Wednesday, which are the store's slowest sales days. On Tuesday
+and Wednesday, if a customer's subtotal is greater than $50, the
+store will discount the customer's purchase by 10%.
+"""
+
+# Import the datatime module so that
+# it can be used in this program.
 from datetime import datetime
+
+# The discount rate is 10% and the sales tax rate is 6%.
+DISC_RATE = 0.10
+SALES_TAX_RATE = 0.06
+
+subtotal = 0
+
+# Print statement for the user
+print('Enter the price and quantity for each item.')
+
+#Declare a loop until the price is equal to 0
+price = 1
+while price != 0:
+    # Get the price from the user.
+    price = float(input('Please enter the price: '))
+    if price != 0:
+        # Get the quantity from the user.
+        quantity = int(input('Please enter the quantity: '))
+
+        subtotal += price * quantity
+
+        # Print a blank line.
+        print()
+
+# Round the subtotal to two digits after
+# the decimal and print the subtotal.
+subtotal = round(subtotal, 2)
+print(f'Subtotal: {subtotal:.2f}')
+
+# Print a blank line
+print()
 
 # Call the now() method to get the current
 # date and time as a datetime object from
@@ -8,33 +47,27 @@ current_date_and_time = datetime.now()
 
 # Call the weekday() method to get the day of the
 # week from the current_date_and_time object.
-day_of_week = current_date_and_time.weekday()
+weekday = current_date_and_time.weekday()
 
-# Print the day of the week for the user to see.
-# print(day_of_week)
-
-subtotal = float(input('Please enter the subtotal: '))
-
-
-if subtotal >= 50:
-    if day_of_week == 2 or day_of_week == 3:
-        discount = 0.1
-        discount_amount = subtotal * discount
-        subtotal_after_discount = subtotal - discount_amount
-        tax = subtotal_after_discount * 0.06
-        total = subtotal_after_discount + tax
-        print(f'Discount Amount: {discount_amount:.2f}')
-        print(f'Sales tax amount: {tax:.2f}')
-        print(f'Total: {total:.2f}')
+# if the subtotal is greater than 50 and today is
+# Tuesday or Wednesday, compute the discount amount.
+if weekday == 1 or weekday == 2:
+    if subtotal < 50:
+        lacking = 50 - subtotal
+        print(f'To receive the discount, add {lacking:.2f} to your order.')
     else:
-        discount = 0
-        tax = subtotal * 0.06
-        total = subtotal + tax
-        print(f'Sales tax amount: {tax:.2f}')
-        print(f'Total: {total:.2f}')
-else:
-    discount = 0
-    tax = subtotal * 0.06
-    total = subtotal + tax
-    print(f'Sales tax amount: {tax:.2f}')
-    print(f'Total: {total:.2f}')
+        discount = round(subtotal * DISC_RATE, 2)
+        print(f'Discount amount: {discount:.2f}')
+        subtotal -= discount
+
+# Compute the sales tax. We compute the sales tax
+# after computing the discount because the customer does not
+# pay sales tax on the full price but on the discounted price.
+sales_tax = round(subtotal * SALES_TAX_RATE, 2)
+print(f'Sales tax amount: {sales_tax:.2f}')
+
+# Compute the total by adding the subtotal and the sales tax.
+total = subtotal + sales_tax
+
+# Display the total for the user to see.
+print(f'Total: {total:.2f}')
