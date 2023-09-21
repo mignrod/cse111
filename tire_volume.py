11afe547c9b669cdb5fb6636f6c9571c3bb01aa6
mignrod <mignrod@gmail.com>
2023-09-21 18:35:03 -0400

    assigment

--- tire_volume.py
+++ tire_volume.py
@@ -8,8 +8,9 @@ while they are using a program and then to analyze those values.
 One common way to record values is for a program to store them in a file.
 """
 
-# Import the Math funtions
+# Import the Math funtions and datetime from library
 import math
+from datetime import datetime
 
 # Print a brief description of the program to the user
 print('This program is build to computes')
@@ -30,3 +31,20 @@ tire_volume = round(tire_volume, 2)
 # Print the result from the calculation.
 print()
 print(f'The approximate volume is {tire_volume} liters')
+
+# Get day from the Computer system
+today = datetime.now()
+
+
+# Open a text file named volumes.txt in append mode
+with open('volumes.txt', 'at') as volumes_file:
+
+    # Print on file
+    print(f'Current date: {today:%d-%m-%Y}', file=volumes_file)
+    print(f'Width of tire: {width:.0f}', file=volumes_file)
+    print(f'Aspect ratio of the tire: {ratio:.0f}', file=volumes_file)
+    print(f'Diameter of the wheel: {diameter:.0f}', file=volumes_file)
+    print(f'Volume of the tire: {tire_volume}', file=volumes_file)
+
+    # Print blank space for future append.
+    print('', file=volumes_file)
