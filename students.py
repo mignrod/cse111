b9e0ab97e5201efcb2d2046c314cef2dd5f51a2a
mignrod <mignrod@gmail.com>
2023-11-09 19:48:00 -0400

    Final mileston week 9

--- students.py
+++ students.py
@@ -9,15 +9,26 @@ def main():
     NAME_INDEX = 1
 
     students = read_dictionary('students.csv', I_NUMBER_INDEX)
-    key_student = input('Please enter an I-Number (xxxxxxxxx): ')
+    key_student = input('Please enter an I-Number (xx-xxx-xxxx): ')
 
-    # if key_student is students:
-    if key_student in students:
-        value = students[key_student]
-        name = value[NAME_INDEX]
-        print(name)
+    # Remove the dashes from the user input
+    key_student = key_student.replace('-', '')
+
+    # 
+    if not key_student.isdigit():
+        print('Invalid character in I-Number')
     else:
-        print('No such student')
+        if len(key_student) < 9:
+            print('Invalid I-Number: too few digits')
+        elif len(key_student) > 9:
+            print('Invalid I-Number: too many digits')
+        else:
+            if key_student in students:
+                value = students[key_student]
+                name = value[NAME_INDEX]
+                print(name)
+            else:
+                print('No such student')
 
 
 
@@ -41,18 +52,16 @@ def read_dictionary(filename, key_column_index):
     # to the opened file in a variable named student.
     with open(filename, 'rt') as csv_file:
         reader = csv.reader(csv_file)
+
+        # Skip the firts line
         next(reader)
 
+        # Add every row line into the students dictionary 
         for row_list in reader:
             key = row_list[key_column_index]
             students[key] = row_list
 
     return students
-
-
-
-
-
 
 # If this file is executed like this:
 # > python program.py
