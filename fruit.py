a6b7ffe3acf2559a854b24584bf68e557f8f8d21
mignrod <mignrod@gmail.com>
2023-11-27 23:41:49 -0400

    preparation week 12

---
+++ fruit.py
@@ -0,0 +1,33 @@
+def main():
+    try:
+        # Create and print a list named fruit.
+        fruit_list = ['pear', 'banana', 'apple', 'mango']
+        print(f'original: {fruit_list}')
+
+        fruit_list.reverse()
+        print(f'reversed: {fruit_list}')
+
+        fruit_list.append('orange')
+        print(f'append orange: {fruit_list}')
+
+        index = fruit_list.index('apple')
+        fruit_list.insert(index, 'cherry')
+        print(f'insert cherry: {fruit_list}')
+
+        fruit_list.remove('banana')
+        print(f'remove banana: {fruit_list}')
+
+        last_item = fruit_list.pop()
+        print(f'pop {last_item}: {fruit_list}')
+
+        fruit_list.sort()
+        print(f'sorted list: {fruit_list}')
+
+        fruit_list.clear()
+        print(f'cleared: {fruit_list}')
+
+    except IndexError as index_err:
+        print(type(index_err).__name__, index_err, sep=": ")
+        
+if __name__ == '__main__':
+    main()
